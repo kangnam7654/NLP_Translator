e42584bb583615da41b1db135e166f28cd672a8b
kangnamkim <kangnam7654@naver.com>
2022-05-13 13:27:21 +0900

    Base 학습 지원을 위한 Class 수정

--- dataload/dataloader.py
+++ dataload/dataloader.py
@@ -1,69 +1,41 @@
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader
-from dataload.dataset import CustomDataset, BaseCustomDataset
+from dataload.dataset import CustomDataset
 
 
 class LitDataLoader(LightningDataModule):
-    def __init__(self, train_df=None, valid_df=None, test_df=None, tokenizer=None, device=None):
+    def __init__(self, train_df=None, valid_df=None, test_df=None, tokenizer=None, translator=False):
         super().__init__()
         self.train_df = train_df
         self.valid_df = valid_df
         self.test_df = test_df
         self.tokenizer = tokenizer
-        self.device = device
+        self.translator = translator  # 번역기 학습 여부
 
     def __create_dataset(self, mode='train'):
         if mode == 'train':
-            return CustomDataset(df=self.train_df, tokenizer=self.tokenizer, train_stage=True)
+            return CustomDataset(df=self.train_df, tokenizer=self.tokenizer, train_stage=True, translator=self.translator)
         elif mode == 'valid':
-            return CustomDataset(df=self.valid_df, tokenizer=self.tokenizer, train_stage=True)
+            return CustomDataset(df=self.valid_df, tokenizer=self.tokenizer, train_stage=True, translator=self.translator)
         elif mode == 'test':
-            return CustomDataset(df=self.test_df, tokenizer=self.tokenizer, train_stage=False)
+            return CustomDataset(df=self.test_df, tokenizer=self.tokenizer, train_stage=False, translator=self.translator)
 
-    def train_dataloader(self):
+    def train_dataloader(self, batch_size=6, shuffle=True, drop_last=False, num_workers=4):
         dataset = self.__create_dataset('train')
-        return DataLoader(dataset, batch_size=6, shuffle=True, drop_last=False, num_workers=4)
+        return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
 
-    def val_dataloader(self):
+    def val_dataloader(self, batch_size=6, shuffle=False, drop_last=False, num_workers=4):
         dataset = self.__create_dataset('valid')
-        return DataLoader(dataset, batch_size=6, shuffle=False, drop_last=False, num_workers=4)
+        return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
 
     def predict_dataloader(self):
         raise Exception('predict_dataloader는 구현되어 있지 않습니다. test_dataloader를 이용해주세요.')
 
     def test_dataloader(self):  # 이번 프로젝트에선 사용 안하므로 삭제 대기
         dataset = self.__create_dataset('test')
+        # 수정 요망
         return DataLoader(dataset, batch_size=self.cfg['TEST']['BATCH_SIZE'], shuffle=False, drop_last=False, num_workers=8, pin_memory=True)
 
 
-class BaseLitDataLoader(LightningDataModule):
-    def __init__(self, train_df=None, valid_df=None, test_df=None, tokenizer=None, device=None):
-        super().__init__()
-        self.train_df = train_df
-        self.valid_df = valid_df
-        self.test_df = test_df
-        self.tokenizer = tokenizer
-        self.device = device
-
-    def __create_dataset(self, mode='train'):
-        if mode == 'train':
-            return BaseCustomDataset(df=self.train_df, tokenizer=self.tokenizer, train_stage=True)
-        elif mode == 'valid':
-            return BaseCustomDataset(df=self.valid_df, tokenizer=self.tokenizer, train_stage=True)
-        elif mode == 'test':
-            return BaseCustomDataset(df=self.test_df, tokenizer=self.tokenizer, train_stage=False)
-
-    def train_dataloader(self):
-        dataset = self.__create_dataset('train')
-        return DataLoader(dataset, batch_size=6, shuffle=True, drop_last=False, num_workers=4)
-
-    def val_dataloader(self):
-        dataset = self.__create_dataset('valid')
-        return DataLoader(dataset, batch_size=6, shuffle=False, drop_last=False, num_workers=4)
-
-    def predict_dataloader(self):
-        raise Exception('predict_dataloader는 구현되어 있지 않습니다. test_dataloader를 이용해주세요.')
-
-    def test_dataloader(self):  # 이번 프로젝트에선 사용 안하므로 삭제 대기
-        dataset = self.__create_dataset('test')
-        return DataLoader(dataset, batch_size=self.cfg['TEST']['BATCH_SIZE'], shuffle=False, drop_last=False, num_workers=8, pin_memory=True)
+if __name__ == '__main__':
+    pass
--- dataload/dataset.py
+++ dataload/dataset.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class CustomDataset(Dataset):
-    def __init__(self, df, tokenizer, max_len=256, pad_index=1, ignore_index=-100, train_stage=True):
+    def __init__(self, df, tokenizer, max_len=256, pad_index=1, ignore_index=-100, train_stage=True, translator=False):
         super().__init__()
         self.df = df
         self.tokenizer = tokenizer
@@ -11,6 +11,7 @@ class CustomDataset(Dataset):
         self.max_len = max_len
         self.pad_index = pad_index
         self.ignore_index = ignore_index
+        self.translator = translator
 
     def __len__(self):
         return len(self.df)
@@ -20,14 +21,17 @@ class CustomDataset(Dataset):
         input_ids = self.tokenizer.encode(instance['원문'])
         input_ids = self.add_padding_data(input_ids)
 
-        label_ids = self.tokenizer.encode(instance['번역문'])
+        if self.translator:  # 번역기 학습
+            label_ids = self.tokenizer.encode(instance['번역문'])
+        else:  # base 학습
+            label_ids = self.tokenizer.encode(instance['원문'])
         label_ids.append(self.tokenizer.eos_token_id)
 
         dec_input_ids = [self.pad_index]
         dec_input_ids += label_ids[:-1]
         dec_input_ids = self.add_padding_data(dec_input_ids)
 
-        label_ids = self.add_ignored_data(dec_input_ids)
+        label_ids = self.add_ignored_data(label_ids)
 
         return {'input_ids': np.array(input_ids),
                 'decoder_input_ids': np.array(dec_input_ids),
@@ -49,53 +53,6 @@ class CustomDataset(Dataset):
             inputs = inputs[:self.max_len]
         return inputs
 
-
-class BaseCustomDataset(Dataset):
-    def __init__(self, df, tokenizer, max_len=256, pad_index=1, ignore_index=-100, train_stage=True):
-        super().__init__()
-        self.df = df
-        self.tokenizer = tokenizer
-        self.train_stage = train_stage
-        self.max_len = max_len
-        self.pad_index = pad_index
-        self.ignore_index = ignore_index
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, idx):
-        instance = self.df.iloc[idx]
-        input_ids = self.tokenizer.encode(instance['원문'])
-        input_ids = self.add_padding_data(input_ids)
-
-        label_ids = self.tokenizer.encode(instance['원문'])
-        label_ids.append(self.tokenizer.eos_token_id)
-
-        dec_input_ids = [self.pad_index]
-        dec_input_ids += label_ids[:-1]
-        dec_input_ids = self.add_padding_data(dec_input_ids)
-
-        label_ids = self.add_ignored_data(dec_input_ids)
-
-        return {'input_ids': np.array(input_ids),
-                'decoder_input_ids': np.array(dec_input_ids),
-                'labels': np.array(label_ids)}
-
-    def add_padding_data(self, inputs):
-        if len(inputs) < self.max_len:
-            pad = np.array([self.pad_index] * (self.max_len - len(inputs)))
-            inputs = np.concatenate([inputs, pad])
-        else:
-            inputs = inputs[:self.max_len]
-        return inputs
-
-    def add_ignored_data(self, inputs):
-        if len(inputs) < self.max_len:
-            pad = np.array([self.ignore_index] * (self.max_len - len(inputs)))
-            inputs = np.concatenate([inputs, pad])
-        else:
-            inputs = inputs[:self.max_len]
-        return inputs
 
 
 if __name__ == '__main__':
