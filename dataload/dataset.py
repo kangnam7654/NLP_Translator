52365f349cbe293e6413e48c90b2c66899840cc7
kangnamkim <kangnam7654@naver.com>
2022-05-15 14:36:46 +0900

    feat: dynamic masking add, special token add

--- dataload/dataset.py
+++ dataload/dataset.py
@@ -18,23 +18,39 @@ class CustomDataset(Dataset):
     def __getitem__(self, idx):
         instance = self.df.iloc[idx]
 
-        input_ids = self.tokenizer.encode(instance['원문'])
+        input_ids = [self.tokenizer.bos_token_id]
+        input_ids += self.tokenizer.encode(instance['원문'])
+        input_ids.append(self.tokenizer.eos_token_id)
+        input_ids = self.token_masking(input_ids)
         input_ids = self.add_padding_data(input_ids)
 
+        label_ids = [self.tokenizer.bos_token_id]
         if self.translator:  # 번역기 학습
-            label_ids = self.tokenizer.encode(instance['번역문'])
+            label_ids += self.tokenizer.encode(instance['번역문'])
         else:  # base 학습
-            label_ids = self.tokenizer.encode(instance['원문'])
+            label_ids += self.tokenizer.encode(instance['원문'])
+        label_ids.append(self.tokenizer.eos_token_id)
 
-        dec_input_ids = [self.tokenizer.pad_token_id]
+        dec_input_ids = [self.tokenizer.eos_token_id]
         dec_input_ids += label_ids[:-1]
         dec_input_ids = self.add_padding_data(dec_input_ids)
 
-        label_ids = self.add_ignored_data(label_ids)
+        label_ids = self.add_padding_data(label_ids)
 
         return {'input_ids': np.array(input_ids),
                 'decoder_input_ids': np.array(dec_input_ids),
                 'labels': np.array(label_ids)}
+
+    def token_masking(self, inputs):
+        n_mask = np.random.randint(round(len(inputs)/2))
+        if n_mask < 1:
+            n_mask = 1
+        idx = [i for i in range(len(inputs))]
+        mask_position = np.random.choice(idx, n_mask)
+
+        for i in mask_position:
+            inputs[i] = self.tokenizer.mask_token_id
+        return inputs
 
     def add_padding_data(self, inputs):
         if len(inputs) < self.max_len:
@@ -60,6 +76,9 @@ def test():
     from torch.utils.data import DataLoader
     df = pd.read_csv(GetPaths.get_data_folder('train.tsv'), delimiter='\t')
     tokenizer = get_tokenizer()
+    w = 'ひ'
+    a = tokenizer.encode(w)
+    b = tokenizer.decode(a)
     d_set = DataLoader(CustomDataset(df, tokenizer))
     for i in d_set:
         print(i)
