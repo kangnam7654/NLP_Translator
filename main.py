6ecb036f1ce7da8ac4b2ae6d8932b66c46a31486
kangnamkim <kangnam7654@naver.com>
2022-05-17 19:07:28 +0900

    fix: example

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ def main():
 
     # config, device = global_setting('cfg.yaml')
     model = Bart()  # 모델
+    model.apply_ckpt('./ckpt/model.ckpt')
     tokenizer = model.tokenizer  # 토크나이저
 
     # 데이터 프레임
     csv = pd.read_csv(GetPaths.get_data_folder('train.tsv'), delimiter='\t')  # tsv 읽어오기
-    # csv = csv[:50000]
+    csv = csv[:30000]
     train_df, valid_df = train_test_split(csv, test_size=0.1)  # train, valid 나누기
     train_df.reset_index(drop=True, inplace=True)  # Index 리셋
     valid_df.reset_index(drop=True, inplace=True)  # Index 리셋
@@ -30,8 +31,8 @@ def main():
                                 tokenizer=tokenizer,
                                 translator=False)
 
-    train_loader = lit_loaders.train_dataloader()  # 학습 Data Loader
-    valid_loader = lit_loaders.val_dataloader()  # 검증 Data Loader
+    train_loader = lit_loaders.train_dataloader(num_workers=6)  # 학습 Data Loader
+    valid_loader = lit_loaders.val_dataloader(num_workers=6)  # 검증 Data Loader
 
     # 콜백
     csv_logger = pl_loggers.CSVLogger(save_dir='./logs/', name='')
