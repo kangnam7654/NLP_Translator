14bec1a35cf2a2212d259bdc45028df386fae495
kangnamkim <kangnam7654@naver.com>
2022-05-25 18:11:59 +0900

    Fix: text infilling 수정

--- utils/text_infilling.py
+++ utils/text_infilling.py
@@ -1,45 +1,29 @@
-import math
 import numpy as np
 
 
 class TextInfilling:
-    def __init__(self, text, mask_token_id=-1):
-        self.text = text
-        self.n = len(text)
-        self.probabilities = self.get_probabilities()
-        self.span = self.get_span()
-        self.mask_token_id = mask_token_id
-        self.masked_text = self.mask()
+    def __init__(self, mask_token_id=4, return_list=True):
+        self._return_list = return_list
+        self.mask_token_id = [mask_token_id]
 
-    @staticmethod
-    def poisson_distribution(n, lamb=3):
-        p_n = (lamb**n)*math.exp(-lamb)/math.factorial(n)
-        if p_n < 0:
-            return 0
+    def mask(self, text):
+        len_text = len(text)
+        span = np.random.poisson(3)
+        while span >= len_text:
+            span = np.random.poisson(3)
+        mask_start_idx = np.random.choice(len_text-span)
+        mask_end_idx = mask_start_idx + span
+
+        start_cut = text[:mask_start_idx]
+        end_cut = text[mask_end_idx:]
+        result = np.concatenate([start_cut, self.mask_token_id, end_cut])
+        if self._return_list:
+            return list(result)
         else:
-            return p_n
-
-    def get_probabilities(self):
-        tmp = np.arange(self.n)
-        probabilities = []
-        for idx in tmp:
-            probabilities.append(self.poisson_distribution(idx))
-        return probabilities
-
-    def get_span(self):
-        span = np.random.choice(self.n, p=self.probabilities)
-        return span
-
-    def mask(self):
-        mask_start_idx = np.random.choice(self.n-self.span)
-        mask_end_idx = mask_start_idx + self.span
-
-        start_cut = self.text[:mask_start_idx]
-        end_cut = self.text[mask_end_idx:]
-        result = np.concatenate([start_cut, [self.mask_token_id], end_cut])
-        return result
+            return result
 
 
 if __name__ == '__main__':
     sample = list(np.arange(30))
-    text_infilling = TextInfilling(sample)
+    text_infilling = TextInfilling()
+    masked = text_infilling.mask(sample)
