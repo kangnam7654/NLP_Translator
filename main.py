e76dff1b3ce6b09375c0ecde608054a938fd47b8
kangnamkim <kangnam7654@naver.com>
2022-05-25 18:12:17 +0900

    Fix:

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
-
+from utils.lr_finder import custom_lr_finder
+from utils.config_reader import cfg_load
 from pytorch_lightning import Trainer
-from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks.lr_monitor import LearningRateMonitor
 from model.bart_model import Bart
 from utils.common.project_paths import GetPaths
 from dataload.dataloader import LitDataLoader
@@ -13,14 +15,14 @@ from sklearn.model_selection import train_test_split
 def main():
     # 모델 및 토크나이저
 
-    # config, device = global_setting('cfg.yaml')
+    config = cfg_load()
     model = Bart()  # 모델
-    model.apply_ckpt('./ckpt/model.ckpt')
+    # model.apply_ckpt('./ckpt/model.ckpt')
     tokenizer = model.tokenizer  # 토크나이저
 
     # 데이터 프레임
     csv = pd.read_csv(GetPaths.get_data_folder('train.tsv'), delimiter='\t')  # tsv 읽어오기
-    csv = csv[:30000]
+
     train_df, valid_df = train_test_split(csv, test_size=0.1)  # train, valid 나누기
     train_df.reset_index(drop=True, inplace=True)  # Index 리셋
     valid_df.reset_index(drop=True, inplace=True)  # Index 리셋
@@ -31,11 +33,13 @@ def main():
                                 tokenizer=tokenizer,
                                 translator=False)
 
-    train_loader = lit_loaders.train_dataloader(num_workers=6)  # 학습 Data Loader
-    valid_loader = lit_loaders.val_dataloader(num_workers=6)  # 검증 Data Loader
+    train_loader = lit_loaders.train_dataloader(num_workers=4)  # 학습 Data Loader
+    valid_loader = lit_loaders.val_dataloader(num_workers=4)  # 검증 Data Loader
+
+    # Logger
+    wandb_logger = WandbLogger(project='kang_bart')
 
     # 콜백
-    csv_logger = pl_loggers.CSVLogger(save_dir='./logs/', name='')
     ckpt_callback = ModelCheckpoint(dirpath='./ckpt',
                                     filename='./model',
                                     monitor='valid_loss',
@@ -45,16 +49,23 @@ def main():
                                     save_last=False,
                                     verbose=True)
 
-    early_stop = EarlyStopping(monitor='valid_loss', verbose=True, patience=10, mode='min')
+    early_stop = EarlyStopping(monitor='valid_loss', verbose=True, patience=10, mode='min')  # early stopping
+    lr_monitor = LearningRateMonitor()  # logger에 lr 추가
+
+    # 학습률 찾기 및 학습률 갱신
+    # new_lr = custom_lr_finder(model, train_loader)
+    # model.lr = new_lr
 
     # 학습
     trainer = Trainer(max_epochs=100,
                       accelerator='gpu',
                       gpus=1,
-                      logger=csv_logger,
-                      callbacks=[ckpt_callback, early_stop]
+                      logger=wandb_logger,
+                      callbacks=[ckpt_callback, early_stop, lr_monitor],
+                      precision=16
                       )
 
+    wandb_logger.watch(model)
     trainer.fit(model, train_loader, valid_loader)
 
 
